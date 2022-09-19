5424bf74a6f12dff742ef8192a2a7278c58e980e
aditnanda <aditya.nanda0030@gmail.com>
2022-09-19 19:39:31 +0700

    fix barcode with 103 paper size

--- resources/views/produk/barcode.blade.php
+++ resources/views/produk/barcode.blade.php
@@ -10,30 +10,55 @@
         .text-center {
             text-align: center;
         }
+        
+        @page { margin-top: 0px; }
+        body { margin-top: 0px; }
+        
+        .page_break { page-break-before: always; }
+
     </style>
 </head>
 <body>
     <table width="100%">
         <tr>
+            @php
+            $nand = 0;
+            @endphp
             @foreach ($dataproduk as $key => $produk)
 
                 @if ($dataprodukqr[$key])
                     @for ($i = 0; $i < $dataprodukqr[$key]; $i++)
+                    @php
+                        $nand++;
+                        @endphp
                         <td class="text-center" style="border: 1px solid #333;">
-                            <p>{{ $produk->nama_produk }} - Rp. {{ format_uang($produk->harga_jual) }}</p>
+                            <p style="margin-top:4px;">{{ $produk->nama_produk }} - Rp. {{ format_uang($produk->harga_jual) }}</p>
                             <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($produk->kode_produk, 'C39') }}" 
                                 alt="{{ $produk->kode_produk }}"
                                 width="180"
-                                height="60">
+                                height="50">
                             <br>
-                            {{ $produk->kode_produk }}
+                            
+                            <p style="margin-bottom:10px;margin-top:2px;">{{ $produk->kode_produk }}</p>
                         </td>
                         @if ($no++ % 3 == 0)
+                        @if($nand == 12)
+                            </tr>
+                            <div class="page_break"></div>
+                             @php
+                            $nand = 0;
+                            @endphp
+                            <tr>
+                        @else
+                        
+                        
+                        
                             </tr><tr>
                         @endif
+                        @endif
+                        
                     @endfor
                 @endif
-            
                 
             @endforeach
         </tr>
